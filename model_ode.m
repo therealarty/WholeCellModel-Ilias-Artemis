1d1c7e18068f0e9aefcf7d336dd146e08454781e
therealarty <artemis.llamosi@gmail.com>
2015-01-19 15:18:55 +0100

    lambda in odes

--- model_ode.m
+++ model_ode.m
@@ -37,8 +37,9 @@ ddtc_q       = 0;
 
 %% for each reaction - add up contributions to rates of changes
 
+lambda=ComputeLambda(a,r,t,m,q,c_r,c_t,c_m,c_q,k);
+
 k.s %nutrient concentration
-k.lambda %growth rate
 k.ns % nutrient effiency
 k.vt %transport
 k.Kt %Transport
@@ -64,44 +65,44 @@ k.dm %mRNA degradation rate
 vimp=Hillfun(k.vt,k.Kt,k.s);
 vcat=Hillfun(k.vm,k.Km,s_i);
 
-ddts_i     = ddts_i  + vimp*e_t-vcat*e_m-k.lambda*s_i;
+ddts_i     = ddts_i  + vimp*e_t-vcat*e_m-lambda*s_i;
 
 ddta       = ddta + k.ns*vcat*e_m;
 ddta       = ddta - c_r*Hillfun(k.gamma_max,k.K_gamma,a);% multiply and divide by nx
 ddta       = ddta - c_t*Hillfun(k.gamma_max,k.K_gamma,a);
 ddta       = ddta - c_m*Hillfun(k.gamma_max,k.K_gamma,a);
 ddta       = ddta - c_q*Hillfun(k.gamma_max,k.K_gamma,a);
-ddta       = ddta -k.lambda*a;
+ddta       = ddta -lambda*a;
 
-ddtr = ddtr + c_r/k.nr*Hillfun(k.gamma_max,k.K_gamma,a) -k.lambda*r;
+ddtr = ddtr + c_r/k.nr*Hillfun(k.gamma_max,k.K_gamma,a) -lambda*r;
 ddtr = ddtr + c_r/k.nr*Hillfun(k.gamma_max,k.K_gamma,a)-k.k_b*r*m_r+k.k_u*c_r;
 ddtr = ddtr + c_t/k.nt*Hillfun(k.gamma_max,k.K_gamma,a)-k.k_b*r*m_t+k.k_u*c_t;
 ddtr = ddtr + c_m/k.nm*Hillfun(k.gamma_max,k.K_gamma,a)-k.k_b*r*m_m+k.k_u*c_m;
 ddtr = ddtr + c_q/k.nq*Hillfun(k.gamma_max,k.K_gamma,a)-k.k_b*r*m_q+k.k_u*c_q;
 
-ddte_t = ddte_t + Vtruc(c_t,a,k,'t')-k.lambda*e_t;
+ddte_t = ddte_t + Vtruc(c_t,a,k,'t')-lambda*e_t;
 
-ddte_m = ddte_m + Vtruc(c_m,a,k,'m')-k.lambda*e_m;
+ddte_m = ddte_m + Vtruc(c_m,a,k,'m')-lambda*e_m;
 
-ddtq = ddtq + Vtruc(c_q,a,k,'q')-k.lambda*q;
+ddtq = ddtq + Vtruc(c_q,a,k,'q')-lambda*q;
 
-ddtq = ddtq + Vtruc(c_q,a,k,'q')-k.lambda*q;
+ddtq = ddtq + Vtruc(c_q,a,k,'q')-lambda*q;
 
-ddtm_r = ddtm_r + omega_r(a,k)-(k.lambda + k.dm)*m_r + Vtruc(c_r,a,k,'r')-k.k_b*r*m_r+k.k_u*c_r;
+ddtm_r = ddtm_r + omega_r(a,k)-(lambda + k.dm)*m_r + Vtruc(c_r,a,k,'r')-k.k_b*r*m_r+k.k_u*c_r;
 
-ddtm_t = ddtm_t + omega_t(a,k)-(k.lambda + k.dm)*m_t + Vtruc(c_t,a,k,'t')-k.k_b*r*m_t+k.k_u*c_t;
+ddtm_t = ddtm_t + omega_t(a,k)-(lambda + k.dm)*m_t + Vtruc(c_t,a,k,'t')-k.k_b*r*m_t+k.k_u*c_t;
 
-ddtm_m = ddtm_m + omega_m(a,k)-(k.lambda + k.dm)*m_m + Vtruc(c_m,a,k,'m')-k.k_b*r*m_m+k.k_u*c_m;
+ddtm_m = ddtm_m + omega_m(a,k)-(lambda + k.dm)*m_m + Vtruc(c_m,a,k,'m')-k.k_b*r*m_m+k.k_u*c_m;
 
-ddtm_q = ddtm_q + omega_q(a,k,q)-(k.lambda + k.dm)*m_q + Vtruc(c_q,a,k,'q')-k.k_b*r*m_q+k.k_u*c_q;
+ddtm_q = ddtm_q + omega_q(a,k,q)-(lambda + k.dm)*m_q + Vtruc(c_q,a,k,'q')-k.k_b*r*m_q+k.k_u*c_q;
 
-ddtc_r = ddtc_r -k.lambda*c_r - Vtruc(c_r,a,k,'r')+k.k_b*r*m_r-k.k_u*c_r;
+ddtc_r = ddtc_r -lambda*c_r - Vtruc(c_r,a,k,'r')+k.k_b*r*m_r-k.k_u*c_r;
 
-ddtc_t = ddtc_t -k.lambda*c_t - Vtruc(c_t,a,k,'t')+k.k_b*r*m_t-k.k_u*c_t;
+ddtc_t = ddtc_t -lambda*c_t - Vtruc(c_t,a,k,'t')+k.k_b*r*m_t-k.k_u*c_t;
 
-ddtc_m = ddtc_m -k.lambda*c_m - Vtruc(c_m,a,k,'m')+k.k_b*r*m_m-k.k_u*c_m;
+ddtc_m = ddtc_m -lambda*c_m - Vtruc(c_m,a,k,'m')+k.k_b*r*m_m-k.k_u*c_m;
 
-ddtc_q = ddtc_q -k.lambda*c_q - Vtruc(c_q,a,k,'q')+k.k_b*r*m_q-k.k_u*c_q;
+ddtc_q = ddtc_q -lambda*c_q - Vtruc(c_q,a,k,'q')+k.k_b*r*m_q-k.k_u*c_q;
 
 
 %% output vector of rates of change of all variables
